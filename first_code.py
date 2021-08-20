6761879eb7837187dacc8b768e9c4a9a1ce49725
saikiran <saikiran999777@gmail.com>
2021-08-20 11:37:23 +0530

    function developemet added

--- first_code.py
+++ first_code.py
@@ -1,6 +1,13 @@
-user = input('enter your opinion yes/no: ')
-if user == 'yes':
-    print('enjoy ur treat')
-else:
-    print('make your own treat')
-    print('and make sure it will touch your soul')
+
+def work():
+    print('---->>>>fun')
+    user = input('enter your opinion yes/no: ')
+    if user == 'yes':
+        print('enjoy ur treat')
+    else:
+        print('make your own treat')
+        print('and make sure it will touch your soul')
+        work()
+
+
+work()
