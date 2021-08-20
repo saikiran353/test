959591ef6423af17b88539a4e7e31d5e6115fabc
saikiran <saikiran999777@gmail.com>
2021-08-20 11:45:07 +0530

    development done for user

--- first_code.py
+++ first_code.py
@@ -1,13 +1,12 @@
 
-def work():
-    print('---->>>>fun')
-    user = input('enter your opinion yes/no: ')
-    if user == 'yes':
-        print('enjoy ur treat')
-    else:
-        print('make your own treat')
-        print('and make sure it will touch your soul')
-        work()
+print('---->>>>fun')
+user = input('enter your opinion yes/no: ')
+if user == 'yes':
+    print('enjoy ur treat')
+else:
+    print('make your own treat')
+    print('and make sure it will touch your soul')
+    print('do what u really need')
 
 
-work()
+
