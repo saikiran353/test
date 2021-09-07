2c64b8e071dd23b6212369e381ab8d2bf39150b6
saikiran <saikiran999777@gmail.com>
2021-09-07 18:02:17 +0530

    job seeker state added

--- first_code.py
+++ first_code.py
@@ -3,6 +3,8 @@ print('---->>>>fun')
 user = input('enter your opinion yes/no: ')
 if user == 'yes':
     print('enjoy ur treat')
+    print('first get it by ur self')
+    print('then create ur new path to show others what u fell')
 else:
     print('make your own treat')
     print('and make sure it will touch your soul')
